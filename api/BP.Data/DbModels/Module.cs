1e97a2a2281aded835ad6453150377c6e7998ce5
Nicolas Ondráš <nicolasko999@gmail.com>
2023-04-23 12:39:31 +0200

    add unique id

--- api/BP.Data/DbModels/Module.cs
+++ api/BP.Data/DbModels/Module.cs
@@ -1,10 +1,14 @@
+using Microsoft.EntityFrameworkCore;
+
 namespace BP.Data.DbModels;
 
+[Index(nameof(UniqueId), IsUnique = true)]
 public class Module
 {
     public int Id { get; set; }
     public string Name { get; set; }
-    public int LocationId { get; set; }
+    public string UniqueId { get; set; }
+    public int? LocationId { get; set; }
     public Location Location { get; set; }
     public string Description { get; set; }
     public List<Sensor> Sensors { get; set; }
--- api/BP.Data/DbModels/Sensor.cs
+++ api/BP.Data/DbModels/Sensor.cs
@@ -5,6 +5,7 @@ public class Sensor
     public int Id { get; set; }
     public int ModuleId { get; set; }
     public Module Module { get; set; }
+    public string UniqueId { get; set; }
     public string Name { get; set; }
     public string Description { get; set; }
     public string Unit { get; set; }
