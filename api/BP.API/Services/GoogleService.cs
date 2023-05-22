6bbc6a96cf4e00a224a143c74694124f2e8e9fed
Nicolas Ondráš <nicolasko999@gmail.com>
2023-05-23 01:36:42 +0200

    Improve google geoloc

--- api/BP.API/Services/GoogleService.cs
+++ api/BP.API/Services/GoogleService.cs
@@ -1,4 +1,7 @@
-﻿using BP.API.Utility;
+﻿using System.Collections.Specialized;
+using System.Globalization;
+using System.Web;
+using BP.API.Utility;
 using BP.Data.Models.Geocode;
 using Location = BP.Data.Models.Google.Location;
 
@@ -17,18 +20,45 @@ public class GoogleService
 
     public async Task<Location?> GetLocation(double latitude, double longitude)
     {
+        var reqQuery = new NameValueCollection
+        {
+            {"lat", latitude.ToString(CultureInfo.InvariantCulture)},
+            {"lng", longitude.ToString(CultureInfo.InvariantCulture)}
+        };
+
+        return await GetLocationFromResponse(reqQuery);
+    }
+
+    public async Task<Location?> GetLocation(string address)
+    {
+        var reqQuery = new NameValueCollection
+        {
+            {"address", address}
+        };
+
+        return await GetLocationFromResponse(reqQuery);
+    }
+
+    private async Task<Location?> GetLocationFromResponse(NameValueCollection reqQuery)
+    {
         var apiKey = _configuration["APIKeys:Google"];
 
-        var locationResponse = await Requests.Get<GeocodeResponse>(
-            $"https://maps.googleapis.com/maps/api/geocode/json?latlng={latitude},{longitude}&key={apiKey}");
+        var request = new UriBuilder("https://maps.googleapis.com/maps/api/geocode/json");
+
+        var query = HttpUtility.ParseQueryString(request.Query);
+        query["key"] = apiKey;
+        query.Add(reqQuery);
+        
+        request.Query = query.ToString();
+
+        var locationResponse = await Requests.Get<GeocodeResponse>(request.ToString());
         if (locationResponse == null || locationResponse.results.Count == 0)
-            _logger.LogWarning("SensorCommunityService: Failed to get location for lat {Latitude} and long {Longitude}",
-                latitude, longitude);
+            _logger.LogWarning("SensorCommunityService: Failed to get location");
         else
             return new Location
             {
-                Latitude = latitude,
-                Longitude = longitude,
+                Latitude = locationResponse.results[0].geometry.location.lat,
+                Longitude = locationResponse.results[0].geometry.location.lng,
                 Address = locationResponse.results[0].formatted_address,
                 StreetName =
                     locationResponse.results[0].address_components.FirstOrDefault(a => a.types.Contains("route"))
