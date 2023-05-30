f7fe78bf88929f66e9a6b230c3029d1c5f564405
Nicolas Ondráš <nicolasko999@gmail.com>
2023-05-31 00:30:58 +0200

    Add generic dtos

---
+++ api/BP.Data/Dto/Response/Stats/BasicStatModule.cs
@@ -0,0 +1,9 @@
+﻿namespace BP.Data.Dto.Response.Stats;
+
+public class BasicStatModule
+{
+    public ModuleDto Module { get; set; }
+    public decimal? Current { get; set; }
+    public decimal? Max { get; set; }
+    public decimal? Min { get; set; }
+}
---
+++ api/BP.Data/Dto/Response/Stats/BasicStatsResponse.cs
@@ -0,0 +1,6 @@
+﻿namespace BP.Data.Dto.Response.Stats;
+
+public class BasicStatsResponse
+{
+    public List<BasicStatModule> Modules { get; set; } = new();
+}
